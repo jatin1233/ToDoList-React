6d24f2f9d6b0b5a31b4bbbbe782840122e1ee46e
Jatin Sharma <jatin.sharma636@gmail.com>
2021-08-23 12:22:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./component/Header";
+import Content from "./component/Content";
+import "./App.scss";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Content />
+    </>
+  );
+}
+
+export default App;
